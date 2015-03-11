d2b23686b473f0162ba165b2ee33ead9d37a46bc
Justin Cook <justin.cook@digitalsurgeons.com>
2015-03-11 17:32:58 -0400

    changed org file

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -12,7 +12,7 @@
   (myorg-update-parent-cookie))
 
 (setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-default-notes-file (concat org-directory "/main.org"))
 (define-key global-map (kbd "M-<f6>") 'org-capture)
 
 (provide 'setup-org)
